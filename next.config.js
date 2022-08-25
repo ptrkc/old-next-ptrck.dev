1452669ae828f6e7970e107c110a593143bf68cb
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2022-08-25 15:54:30 -0300

    add redirects to linkedin and github

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
+  experimental: { images: { allowFutureImage: true } },
   eslint: {
     dirs: [
       'components',
@@ -15,6 +11,20 @@ const nextConfig = {
       'styles',
     ],
   },
-}
+  async redirects() {
+    return [
+      {
+        source: '/in',
+        destination: 'https://www.linkedin.com/in/patrickcarneiro',
+        permanent: false,
+      },
+      {
+        source: '/gh',
+        destination: 'https://github.com/ptrkc',
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
