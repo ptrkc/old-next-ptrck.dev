4af9516fc2cb89748fb2bb177b7263e7d9695c3d
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2022-09-29 21:52:06 -0300

    active header links underlined

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 import Link from 'next/link';
 
-const NavLink = ({ href, children } : PropsWithChildren<{href: string}>) => (
+const FooterLink = ({ href, children } : PropsWithChildren<{href: string}>) => (
   <Link className="hover:underline p-2 font-bold" href={href}>
     {children}
   </Link>
@@ -14,16 +14,16 @@ const Footer = () => (
   <footer className="p-8 bg-neutral-800 flex justify-around items-center flex-col gap-8 text-orange-100">
     <div className="mx-auto max-w-3xl flex flex-col sm:flex-row gap-8">
       <ul className="flex flex-col gap-2">
-        <li><NavLink href="/">Home</NavLink></li>
-        <li><NavLink href="/about">About</NavLink></li>
-        <li><NavLink href="/blog">Blog</NavLink></li>
-        <li><NavLink href="/projects">Projects</NavLink></li>
+        <li><FooterLink href="/">Home</FooterLink></li>
+        <li><FooterLink href="/about">About</FooterLink></li>
+        <li><FooterLink href="/blog">Blog</FooterLink></li>
+        <li><FooterLink href="/projects">Projects</FooterLink></li>
       </ul>
       <Divider />
       <ul className="flex flex-col gap-2">
-        <li><NavLink href="https://github.com/ptrkc">GitHub @ptrkc</NavLink></li>
-        <li><NavLink href="https://www.linkedin.com/in/patrickcarneiro/">LinkedIn @patrickcarneiro</NavLink></li>
-        <li><NavLink href="mailto:pcarneiro.dev@gmail.com">pcarneiro.dev@gmail.com</NavLink></li>
+        <li><FooterLink href="https://github.com/ptrkc">GitHub @ptrkc</FooterLink></li>
+        <li><FooterLink href="https://www.linkedin.com/in/patrickcarneiro/">LinkedIn @patrickcarneiro</FooterLink></li>
+        <li><FooterLink href="mailto:pcarneiro.dev@gmail.com">pcarneiro.dev@gmail.com</FooterLink></li>
       </ul>
     </div>
     © Patrick Carneiro 2022
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,36 @@
 import Link from 'next/link';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import DarkModeToggle from 'components/DarkModeToggle';
+import { useRouter } from 'next/router';
+import cn from 'utils/classnames';
 
-const NavLink = ({ href, children } : PropsWithChildren<{href: string}>) => (
-  <Link className="hover:underline p-2 font-bold" href={href}>
-    {children}
-  </Link>
-);
+const HeaderLink = ({ href, children } : PropsWithChildren<{href: string}>) => {
+  const { pathname } = useRouter();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (pathname.toLowerCase() === href) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }, [pathname, href]);
+
+  return (
+    <Link className={cn('hover:underline p-2 font-bold', isActive && 'underline')} href={href}>
+      {children}
+    </Link>
+  );
+};
 
 const Header = () => (
   <div className="h-14">
     <nav className="p-2 fixed top-0 left-0 right-0 z-10 bg-orange-100 dark:bg-neutral-800 duration-200 backdrop-blur [@supports(backdrop-filter:blur())]:bg-orange-100/50 dark:[@supports(backdrop-filter:blur())]:bg-neutral-900/50">
       <div className="mx-auto max-w-3xl flex justify-around items-center">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/about">About</NavLink>
-        <NavLink href="/blog">Blog</NavLink>
-        <NavLink href="/projects">Projects</NavLink>
+        <HeaderLink href="/">Home</HeaderLink>
+        <HeaderLink href="/about">About</HeaderLink>
+        <HeaderLink href="/blog">Blog</HeaderLink>
+        <HeaderLink href="/projects">Projects</HeaderLink>
         <DarkModeToggle />
       </div>
     </nav>
