d3dc269ac5d6268a41f0fd6840e41a3879233c97
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2022-11-16 02:38:11 -0300

    noise on bg image, no svg

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -47,17 +47,6 @@ const DefaultLayout = ({ children } : PropsWithChildren) => (
       <title>Patrick Carneiro - Software Developer</title>
       <meta name="description" content="Personal blog and portfolio." />
     </Head>
-    <svg className="fixed h-screen w-full mix-blend-soft-light pointer-events-none z-20" xmlns="http://www.w3.org/2000/svg">
-      <filter id="noiseFilter">
-        <feTurbulence
-          type="fractalNoise"
-          baseFrequency="0.90"
-          numOctaves="3"
-          stitchTiles="stitch"
-        />
-      </filter>
-      <rect width="100%" height="100%" filter="url(#noiseFilter)" />
-    </svg>
     <Header />
     <Transition>
       <main className="p-4 mx-auto max-w-4xl">
